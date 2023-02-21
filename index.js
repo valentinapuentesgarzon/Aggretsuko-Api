8f63d1ef71bf9f21bc15633b66e18af436673d92
valentinapuentesgarzon <valentinapuentes@gmail.com>
2023-02-21 09:42:58 -0500

    Conexión puerto

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo')
+const express = require('express')
+const app = express()
+const port = 3020
+
+app.get('/', (req, res) => {
+  res.send('Hola Val')
+})
+
+app.listen(port, ()=> {
+  console.log('Mi puerto es'+ port)
+})
+
